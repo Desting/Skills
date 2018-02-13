42804ee159571262c76cc289f8cb4fa2a9ea38f4
Nikolaj Desting <nik_zd@hotmail.com>
2018-02-13 10:32:04 +0100

    Database created and new collection inserted. We got a whole through!

---
+++ demo_create_mongo_db.js
@@ -0,0 +1,12 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://Desting:UtNCMcq9UzPWdww2@cluster0-pnbrr.mongodb.net/test";
+
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("mydb");
+    dbo.createCollection("customers", function(err, res) {
+      if (err) throw err;
+      console.log("Collection created!");
+      db.close();
+    });
+  });

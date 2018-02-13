75141dc7a755d9e5f7a23cd96f9a835dba46af3e
Nikolaj Desting <nik_zd@hotmail.com>
2018-02-13 08:50:10 +0100

    Managed to cause the log "database created" without being able to see the created DB anywhere.

--- demo_create_mongo_db.js
+++ demo_create_mongo_db.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+var url = 'mongodb+srv://Desting:UtNCMcq9UzPWdww2@cluster0-pnbrr.mongodb.net/niktest';
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
